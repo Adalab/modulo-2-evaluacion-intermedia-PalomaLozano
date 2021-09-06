52b70d73c738bd097b0ca3926b1513a1b98eef97
Paloma Lozano <palomalogu9@gmail.com>
2021-09-06 19:24:26 +0200

    improvement of readability and fix javascript issues

--- js/main.js
+++ js/main.js
@@ -1,72 +1,68 @@
     'use strict';
 
+    const numberX = document.querySelector ('.js_number');
+
     const button = document.querySelector('.js_button');
 
-    const numberX = document.querySelector ('.js_numberX');
+    const clue = document.querySelector('.js_clue');
 
-    const pista = document.querySelector('.js_clue');
+    const attempsEl = document.querySelector('.js_chances');
 
-    const intentos = document.querySelector('.js_chances');
+    let attemps = 0;
 
-   
+    const numberResult = getRandomNumber(100);
 
-
+    const form = document.querySelector('.js-form');
     
-
+    
     function getRandomNumber(max) {
         return Math.ceil(Math.random() * max);
        }
-       
+
+       console.log(numberResult);
     
-    
-    
+    function writeClue(message){
+      clue.innerHTML = message;
+    }
 
     function game (){
-    
-        const numero = getRandomNumber(100);
-        
-        const numeroUser = numberX.value;
+  
+    const numberUser = parseInt(numberX.value);
 
-        const pistaUser = pista.value;
+    if (numberUser <= 0 || numberUser > 100) {
+        writeClue('El número debe estar entre 1 y 100');
+    }
 
-    if (numeroUser === numero ){
+    else if (numberUser > numberResult ){
 
-        pistaUser.innerHTML = 'Has ganado campeona!!!';
+        writeClue('Demasiado alto');
+       }
 
+     else if (numberUser < numberResult ){
+        writeClue('Demasiado bajo');
      }
 
-     else if (numeroUser < numero ){
-        pistaUser.innerHTML = 'Demasiado bajo';
+     else{
+        writeClue('Has ganado campeona!!!');
      }
 
-     else if (numeroUser > numero ){
-        pistaUser.innerHTML = 'Demasiado alto';
-     }
-
-      else if (numeroUser > 100){
-        pistaUser.innerHTML = 'El número debe estar entre 1 y 100';
     }
     
-}
-    
-
-    
-    function contador () {
-        const contadorPrueba = intentos.value;
-        if (contadorPrueba === +1) {
-        intentos.innerHTML = 'contadorPrueba';
-         }
+    function counter () {
+        attemps = attemps +1;
+        attempsEl.value = `Número de intentos: ${attemps}`;
       }
 
-
-
-      
-      function handleButton(){
-        
+    function handleButton(ev){
         game();
-        contador();
-
+        counter();
       }
+      
+    function handleSubmit(ev){
+    ev.preventDefault();
+    }
+   
+    button.addEventListener('click' , handleButton);
 
-
-      button.addEventListener( 'click' , handleButton);
+    form.addEventListener('submit', handleSubmit);
+  
