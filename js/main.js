814f2583396129e36fe4fd7ffd53f5bb55431d66
Paloma Lozano <palomalogu9@gmail.com>
2021-09-21 17:23:37 +0200

    added random colors

--- js/main.js
+++ js/main.js
@@ -1,68 +1,74 @@
-    'use strict';
+'use strict';
 
-    const numberX = document.querySelector ('.js_number');
+const numberX = document.querySelector('.js_number');
 
-    const button = document.querySelector('.js_button');
+const button = document.querySelector('.js_button');
 
-    const clue = document.querySelector('.js_clue');
+const clue = document.querySelector('.js_clue');
 
-    const attempsEl = document.querySelector('.js_chances');
+const attempsEl = document.querySelector('.js_chances');
 
-    let attemps = 0;
+let attemps = 0;
 
-    const numberResult = getRandomNumber(100);
+const numberResult = getRandomNumber(100);
 
-    const form = document.querySelector('.js-form');
-    
-    
-    function getRandomNumber(max) {
-        return Math.ceil(Math.random() * max);
-       }
+const form = document.querySelector('.js-form');
 
-       console.log(numberResult);
-    
-    function writeClue(message){
-      clue.innerHTML = message;
-    }
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
 
-    function game (){
-  
-    const numberUser = parseInt(numberX.value);
+console.log(numberResult);
 
-    if (numberUser <= 0 || numberUser > 100) {
-        writeClue('El número debe estar entre 1 y 100');
-    }
+function writeClue(message) {
+  clue.innerHTML = message;
+}
 
-    else if (numberUser > numberResult ){
+function game() {
+  const numberUser = parseInt(numberX.value);
 
-        writeClue('Demasiado alto');
-       }
+  if (numberUser <= 0 || numberUser > 100) {
+    writeClue('👁‍🗨 The number must be between 1 and 100 👁‍🗨');
+  } else if (numberUser > numberResult) {
+    writeClue('👆 Too high 👆');
+  } else if (numberUser < numberResult) {
+    writeClue('👇 Too low 👇');
+  } else if (numberUser === numberResult) {
+    writeClue('🥇 You win, champion!!! 🥇');
+  } else {
+    writeClue('👽 Must put a number 👽');
+  }
+}
 
-     else if (numberUser < numberResult ){
-        writeClue('Demasiado bajo');
-     }
+function counter() {
+  attemps = attemps + 1;
+  attempsEl.value = `Number of attemps: ${attemps}`;
+}
 
-     else{
-        writeClue('Has ganado campeona!!!');
-     }
+function handleButton(ev) {
+  game();
+  counter();
+  btnRandom();
+}
 
-    }
-    
-    function counter () {
-        attemps = attemps +1;
-        attempsEl.value = `Número de intentos: ${attemps}`;
-      }
+function handleSubmit(ev) {
+  ev.preventDefault();
+}
 
-    function handleButton(ev){
-        game();
-        counter();
-      }
-      
-    function handleSubmit(ev){
-    ev.preventDefault();
-    }
-   
-    button.addEventListener('click' , handleButton);
+button.addEventListener('click', handleButton);
 
-    form.addEventListener('submit', handleSubmit);
-  
+form.addEventListener('submit', handleSubmit);
+
+//randomColor
+
+const btn = document.querySelector('.randomColor');
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function btnRandom() {
+  const rndCol =
+    'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
